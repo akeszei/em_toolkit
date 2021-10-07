49cc9cfa2056877a5ab9cd2ccfc577cce0f6712a
akeszei <alex.keszei@gmail.com>
2021-10-07 09:22:03 -0400

    Update
    Initialize project

---
+++ EPU_tools/reorganize_EPU_data.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+
+## run this script in the parent folder of an EPU session, e.g.
+##      session_2021-08-18/<run here>
+##          /A3g1
+##          /A3g2
+##          ..
+## The script will automatically make new folders arranged in a manner I like, e.g.
+##          /reorganized/A3g1/Raw_data/*.mrc
+##                           /Jpgs/*.jpg
+##
+
+############ Simplify text output modifiers (e.g. echo -e -e -e "${red}color text here,${default} normal text color here");
+## NOTE: tput function can slow down the launch of colorized scripts significantly on some setups and completely fail on installs lacking tput.
+## Escape sequences are much faster and more widely implemented, with the minor drawback of less color control. To implement you require the 'echo -e -e -e' environment. Cannot be used within the 'read' environment unfortunately - requires rebuilding prompt elements
+default='\033[0m' #$(tput sgr0)
+red='\033[0;31m' #$(tput setaf 1)
+green='\033[0;32m' #$(tput setaf 2)
+yellow='\033[0;33m' #$(tput setaf 3)
+blue='\033[0;34m' #$(tput setaf 4)
+magenta='\033[0;35m' #$(tput setaf 5)
+cyan='\033[0;36m' #$(tput setaf 6)
+white='\033[0;37m' #$(tput setaf 7)
+
+
+# echo -e "Color palette: ${red} red ${green} green ${yellow} yellow ${blue} blue ${magenta} magenta ${cyan} cyan ${white} white ${default} default"
+
+## Set a trap to terminate running loops (SIGINT) and processes (SIGTERM) with control+C
+trap "echo -e; echo -e '${red}Script terminated by user.${default}'; exit;" SIGINT SIGTERM
+
+## prepare new parent directory
+if [ ! -d "reorganized" ]; then
+   echo "Files will be passed into new directory: reoganized"
+   mkdir reorganized
+fi
+
+
+## prepare the directory structure for each grid, then run the processing steps
+for grid in $(ls -d */); do
+    ## ignore the directory named 'reorganized'
+    if [[ $grid == "reorganized/" ]]; then
+        continue
+    fi
+
+    echo "EPU directory found: " $grid ## returns folder with a trailing slash (e.g. 'A3g1/')
+
+    ## confirm the directory is potentially a grid by checking if it has *mrc files within it
+    mrc_file_num=$(find $grid -name "*.mrc" | wc -l)
+    if [ $mrc_file_num -eq 0 ]; then
+        echo "Directory found: " ${grid} "but, no mrc files within, skipping..."
+        continue
+    fi
+
+    ## prepare new parent directory
+    if [ ! -d "reorganized/"${grid} ]; then
+       echo "Pepare sub-directory: reoganized/"${grid}
+       mkdir "reorganized/"${grid}
+    fi
+
+    ## prepare sub-directory for exposure images
+    if [ ! -d "reorganized/"${grid}Raw_data ]; then
+       mkdir "reorganized/"${grid}Raw_data
+    fi
+
+    ## prepare sub-directory for search images
+    if [ ! -d "reorganized/"${grid}Jpgs ]; then
+       mkdir "reorganized/"${grid}Jpgs
+    fi
+
+    save_parent_path="reorganized/"${grid} ## parent folder where Raw_data and Jpgs folders will be
+    retrieve_square_path=${grid}"Images-Disc1/" ## path to the folder containing the grid square images
+
+
+    ## check there is an atlas file and convert it to a .jpg if so
+    if [ $(find ${grid}Atlas -name "Atlas*.mrc" | wc -l) -gt 0 ]; then
+        atlas_path=$(find ${grid}Atlas -name "Atlas*.mrc")
+        atlas_basename=${grid%/}"_atlas"
+        echo "  ... preparing atlas at: reorganized/"${grid}"Jpgs/"${atlas_basename}.jpg
+        mrc2img.py $atlas_path ${save_parent_path}"Jpgs/"${atlas_basename}.jpg --bin 1 >> /dev/null
+        mogrify -normalize ${save_parent_path}"Jpgs/"${atlas_basename}.jpg   
+    else
+        echo "No atlas found in " $grid
+    fi
+
+    ## iterate over all grid squares
+    square_counter=0
+    for sq_dir in $(ls -d ${retrieve_square_path}*); do
+        ## advance the counter
+        ((square_counter++))
+        echo "  ... processing grid square #" ${square_counter}
+        ## copy the square .mrc to a .jpg with a new name
+        sq_basename=${grid///}"_sq_"${square_counter}
+        current_sq_mrc=$(ls ${sq_dir}/*.mrc | head -1) ## get the only/first .mrc file in the directory
+        mrc2img.py $current_sq_mrc ${save_parent_path}"Jpgs/"${sq_basename}.jpg --bin 2 >> /dev/null
+        echo "      ... grid square img saved"
+
+        ## while in a grid square directory, iterate over files in its data subdirectory
+        exposure_counter=0
+        for mic in $(ls -f ${sq_dir}/Data/*.mrc); do
+            ## advance the iterator
+            ((exposure_counter++))
+            ## check there is at least one entry or exit the loop
+            if [ $(ls -f ${sq_dir}/Data/*.mrc | wc -l) -eq 0 ]; then
+                echo "      ... no data images for square, skipping "
+                break
+            fi
+            mic_basename=${sq_basename}"_exp_"${exposure_counter}
+            echo -en "\r\033[K          ... processing exp #${exposure_counter}  (${mic##*/})"
+            ## save a .jpg image
+            mrc2img.py $mic ${save_parent_path}"Jpgs/"${mic_basename}.jpg --bin 4 >> /dev/null
+            ## copy .mrc to Raw_data directory with new name
+            cp $mic ${save_parent_path}"Raw_data/"${mic_basename}.mrc
+        done
+        echo -en "\r\033[K          ... processed #${exposure_counter} micrographs"
+        echo ""
+
+    done
+
+done
---
+++ EPU_tools/show_atlas_position.py
@@ -0,0 +1,229 @@
+
+def header_from_xml(input_string):
+    """ By default convension, the names of XML headers for EPU files contains a lot of unnecessary
+        information, this function aims to remove all that unnecessary text and return the header name
+        alone. e.g.:
+            {unnecessary text}headerTitle {} -> headerTitle
+    """
+    output_sting = input_string.split('{')[1].split('}')[1]
+    return output_sting
+
+def image2array(file, DEBUG = True):
+    """
+        Import an image into a grayscal 2d numpy array with values from (0 - 255), where
+            0 == black
+            255 == white
+    """
+    im = PIL_Image.open(file).convert('L') # 'L' == convert to grayscale data
+    # convert image to numpy array
+    im_data = np.asarray(im)
+
+    if DEBUG:
+        print("===================================================")
+        print(" IMPORT IMAGE :: %s" % file)
+        print("===================================================")
+        print("   >> %s px, min = %s, max = %s" % (im_data.shape, np.min(im_data), np.max(im_data)))
+
+    return im_data
+
+def get_tile_files():
+    file_list = []
+    for file in glob.glob("*.xml"):
+        if 'Tile' in file:
+            file_list.append(file)
+    ## organize file list by alpha numeric
+    file_list = sorted(file_list)
+    return file_list
+
+def get_tile_coords(file_list):
+    tile_coordinates = []
+    for xml_file in file_list:
+        tile_coordinates.append(point_from_xml(xml_file))
+
+    print(" >> %s atlas tiles parsed" % len(tile_coordinates))
+    return tile_coordinates
+
+def plot_points(coordinates):
+    """ For a given set of coordinates, add points to an open 'matplotlib.pyplot as plt' canvas
+    PARAMETERS
+        coordinates = [(x1, y1), (x2, y2), ... (xn, yn)]
+    RETURNS
+        void
+    """
+    ## unzip the data and generate two independent lists for each x and y coordinate
+    xs, ys = zip(*coordinates)
+    xs = list(xs)
+    ys = list(ys)
+    ## pass the unzipped values into the scatter function
+    plt.scatter(xs, ys, s = 15, color = 'gray', alpha = 0.5)
+    return
+
+def as_unit_vector(input_vector):
+    """ For an input vector sitting at the origin, normalize its magnitude to make it a unit vector in the range of 0 through 1.
+    PARAMETERS
+        input_vector = np.array([x, y])
+    RETURNS
+        output_vector = np.array([a, b]) whose norm == 1
+    """
+    output_vector = input_vector / np.linalg.norm(input_vector)
+    return output_vector
+
+def point_to_relative_basis_lengths(point, basis_vectors):
+    """ For a given point sitting on a plane defined by two basis vectors of fixed length, determine how many basis-lengths along each basis
+        direction the point lies. The output fold differences can be used to scale the point along a new set of basis vectors later.
+    PARAMETERS
+        point = np.array([x, y]), where (x, y) define a point sitting on a plane
+        basis_vectors = tuple(np.array([x1, y1]), np.array([x2, y2])), where (x1, y1) and (x2, y2) define two basis vectors sitting at a common origin of (0, 0)
+    RETURNS
+    """
+    ## 1. normalize the basis vectors so they range from 0 to 1 (effectively making them abitrarily larger than any point we might consider, and hence not limiting in a dot product calculation)
+    x_unit = as_unit_vector(basis_vectors[0])
+    y_unit = as_unit_vector(basis_vectors[1])
+    # print(" unit vectors for x, y = %s, %s" % (x_unit_real, y_unit_real))
+    ## 2. calculate the inner (i.e. dot) product of the POI along each axis defined by the basis vectors
+    inner_product_x = np.inner(point, x_unit)
+    inner_product_y = np.inner(point, y_unit)
+    # print("inner products along x, y = %s, %s" % (inner_product_x, inner_product_y))
+    ## 3. determine the fold difference of the POI inner products against the magnitude of each basis vector along that axis (since the position of each basis vector is known relative to the final image, we can use this fold difference to rescale the image basis vectors correctly)
+    fold_difference_point2basis_x = inner_product_x / np.linalg.norm(basis_vectors[0])
+    fold_difference_point2basis_y = inner_product_y / np.linalg.norm(basis_vectors[1])
+    print(" percent position of point to basis vectors = %s, %s " % (fold_difference_point2basis_x, fold_difference_point2basis_y))
+
+    return (fold_difference_point2basis_x, fold_difference_point2basis_y)
+
+def point_from_xml(fname):
+    """ For an input EPU .XML file, find the X and Y coordinates of the stage embedded within
+    PARAMETERS
+        fname = str(), name of the file
+    RETURNS
+        coords = tuple(x, y), where x, y correspond to the entries at <X> and <Y> for the stage position
+    """
+    ## load the file into xml data structure
+    xml_data = ET.fromstring(open(fname).read())
+
+    x, y = (None, None)
+    for entry in xml_data:
+        # print("PARENT ENTRIES")
+        # print(header_from_xml(entry.tag))
+        if 'microscopeData' == header_from_xml(entry.tag):
+            # print("MICROSCOPE DATA")
+            for subentry in entry:
+                # print(header_from_xml(subentry.tag))
+                if 'stage' == header_from_xml(subentry.tag):
+                    # print("STAGE DATA")
+                    for subsubentry in subentry:
+                        # print(subsubentry.tag, subsubentry.attrib)
+                        if 'Position' == header_from_xml(subsubentry.tag):
+                            # print("POSITION DATA")
+                            for subsubsubentry in subsubentry:
+                                # print(subsubsubentry.tag, subsubsubentry.attrib)
+                                if 'X' == header_from_xml(subsubsubentry.tag):
+                                    x = float(subsubsubentry.text)
+                                    # print("X = ", x)
+                                if 'Y' in header_from_xml(subsubsubentry.tag):
+                                    y = float(subsubsubentry.text)
+                                    # print("Y = ", y)
+    coords = (x, y)
+    return coords
+
+def find_scaling_factor(tile_num):
+    """ For a given number of tiles composing the EPU atlas, determine the number of basis vector lengths span from the center of the image to an edge.
+    PARAMETERS
+        tile_num = int(), number of tiles of which the atlas is composed
+    RETURNS
+        scaling_factor = float()
+    """
+    if tile_num <= 9:
+        return 1.5
+    elif tile_num <= 25:
+        return 2.5
+    elif tile_num <= 49:
+        return 3.5
+    elif tile_num <= 81:
+        return 4.5
+    elif tile_num <= 121:
+        return 5.5
+    elif tile_num <= 169:
+        return 6.5
+    else:
+        return print("ERROR :: Unexpected number of tiles for atlas (%s)!" % tile_num)
+
+if __name__ == '__main__':
+    from PIL import Image as PIL_Image
+    import numpy as np
+    import xml.etree.ElementTree as ET
+    import glob
+    import sys # for sys.exit() calls while building script
+    ## plot points for sanity checking
+    import matplotlib.pyplot as plt
+
+
+    basename = 'test'
+
+    ## get all .xml files corresponding to Tiles in the atlas
+    tile_files = get_tile_files()
+
+    ## pass each xml file through a parser and get the cached X and Y coordinates
+    tile_coordinates = get_tile_coords(tile_files)
+
+    ## for visualization, plot tile coordinates
+    # plot_points(tile_coordinates)
+
+    ## get the point of interest in real space (the values found in the EPU .XML file)
+    # random_point = np.array([float(7.781908499999994E-05), float(-0.00021753891599999995)]) ## grid square #2
+    # random_point = np.array([float(-0.00012568632000000007), float(8.0770758000000062E-05)]) ## grid square #1
+    # random_point = np.array([float(-0.00014601762000000007), float(0.00033083228400000009)]) ## grid square #3
+    # poi_real = np.array([float(0.00036143162999999992), float(0.00023559537600000008)]) ## grid square #4
+    poi_real = np.array(point_from_xml('GridSquare_20211004_100411.xml'))
+
+    ## for visualization, plot the position of the real POI
+    # plt.scatter(poi_real[0], poi_real[1], s = 30, color = 'red', alpha = 1)
+
+    ## determine the basis vectors for real space
+    real_origin = np.array(tile_coordinates[0])
+
+    ## after trial-and-error, I have determined that EPU atlases can be aligned with real space coordinates by using Tile 3 and 5 as basis vectors representing a fixed step in the positive x and y directions, respectively
+    x_basis_real = np.array(tile_coordinates[3]) - real_origin
+    y_basis_real = np.array(tile_coordinates[5]) - real_origin
+    realspace_basis = (x_basis_real, y_basis_real)
+
+    ## for visualization, plot these basis vectors
+    # plt.plot([real_origin[0], x_basis_real[0]], [real_origin[1], x_basis_real[1]], color='blue', alpha = 0.5)
+    # plt.plot([real_origin[0], y_basis_real[0]], [real_origin[1], y_basis_real[1]], color='blue', alpha = 0.5)
+    # print("Basis vectors = %s, %s)" % (x_basis_real, y_basis_real))
+
+    ## determine for the given point, how much it lies on each axis defined by basis vectors of a fixed (known) length:
+    relative_x, relative_y = point_to_relative_basis_lengths(poi_real, realspace_basis)
+
+    ## load the atlas image
+    im_atlas = image2array('C2g2_atlas.jpg', DEBUG = False)
+    print(" image dimensions = ", im_atlas.shape)
+
+    ## move the image so the origin (0, 0) is at the center
+    ## see: https://stackoverflow.com/questions/34458251/plot-over-an-image-background-in-python
+    left = 0 - int(im_atlas.shape[0] / 2)
+    right = int(im_atlas.shape[0] / 2)
+    bottom = 0 - int(im_atlas.shape[0] / 2)
+    top = int(im_atlas.shape[0] / 2)
+    plt.imshow(im_atlas, cmap = 'gray', extent=[left, right, bottom, top])
+
+    ## set up basis vectors for image space, their magnitude should be directly related to the manitude of the realspace_basis vectors
+    scaling_factor = find_scaling_factor(len(tile_coordinates))
+    x_basis_im = np.array([1, 0]) * int(right / scaling_factor)
+    y_basis_im = np.array([0, 1]) * int(top / scaling_factor)
+    # plt.plot([0, x_basis_im[0]], [0, x_basis_im[1]], color='green', alpha = 0.5)
+    # plt.plot([0, y_basis_im[0]], [0, y_basis_im[1]], color='orange', alpha = 0.5)
+    print(" img basis vectors = %s, %s" % (x_basis_im, y_basis_im))
+
+
+    ## multiply the image-space basis vectors by the determined scaling factor to find the target pixel position of the input point
+    img_pixel_position = (x_basis_im[0] * relative_x, y_basis_im[1] * relative_y)
+    print( "position of remapped pixel coordinate = ", img_pixel_position)
+
+    ## draw a red cicle of arbitrary size centered at the target pixel position
+    plt.plot(img_pixel_position[0], img_pixel_position[1], 'o', markersize = 20, markerfacecolor="None", markeredgecolor = 'tab:red', markeredgewidth=2)
+
+    ## save the image
+    # plt.show()
+    plt.axis('off')
+    plt.savefig(basename + '.jpg', format="jpg", bbox_inches=0, dpi = 350)
