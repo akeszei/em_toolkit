0d6fbf271c1396506f228cb098368853c58cfd79
akeszei <alex.keszei@gmail.com>
2022-06-07 13:28:00 -0400

    Update plot_fsc.py

--- star_file_tools/plot_fsc.py
+++ star_file_tools/plot_fsc.py
@@ -15,7 +15,7 @@ def usage():
 def parse_star_file(file):
     """ For a given postprocess.star file, parse the FSC data into a list of tuples:
                 [
-                    ( inverse_resolution,  Ang_resolution,  FSC_corr,  FSC_unmasked,  FSC_masked),
+                    ( inverse_resolution,  Ang_resolution,  FSC_corr,  FSC_unmasked,  FSC_masked, FSC_randomized, randomize_from),
                     ...
                 ]
     """
@@ -30,7 +30,9 @@ def parse_star_file(file):
         fsc_corr_col = -1
         fsc_unmask_col = -1
         fsc_mask_col = -1
+        fsc_random_col = -1
         parsed_entries = []
+        phase_randomize_from = -1
 
         for line in f :
             line_num += 1
@@ -40,6 +42,12 @@ def parse_star_file(file):
             # ignore empty lines
             if len(line) == 0 :
                 continue
+
+            ## grab the value from the phase randomized entry in the star file  when it appears
+            if '_rlnRandomiseFrom' in line:
+                phase_randomize_from = line_to_list[1]
+                print(" Phase radomize from >> %s Ang" % phase_randomize_from)
+
 
             ## find the start of the data table we are interested in
             if 'data_fsc' in line:
@@ -63,6 +71,10 @@ def parse_star_file(file):
                 if '_rlnFourierShellCorrelationMaskedMaps' in line:
                     fsc_mask_col = get_star_column_number(line)
                     # print('class column = ', line, class_name_col)
+                if '_rlnCorrectedFourierShellCorrelationPhaseRandomizedMaskedMaps' in line:
+                    fsc_random_col = get_star_column_number(line)
+                    # print('class column = ', line, class_name_col)
+
 
             ## catch the end of the table to end this function early
             if (line_num > table_start) and (table_end < table_start):
@@ -74,7 +86,7 @@ def parse_star_file(file):
                     return parsed_entries
 
             ## if we made it this far into the function, we might be on a data entry, run a check first if we have assigned all columns we expect
-            if (inv_res_col > 0) and (ang_res_col > 0) and (fsc_corr_col > 0) and (fsc_unmask_col > 0) and (fsc_mask_col > 0):
+            if (inv_res_col > 0) and (ang_res_col > 0) and (fsc_corr_col > 0) and (fsc_unmask_col > 0) and (fsc_mask_col > 0) and (fsc_random_col > 0):
                 ## filter out any lines that are not data entries based on if they contain '_' or '#' as their first character
                 first_character = ""
                 first_character = list(line_to_list[0])[0]
@@ -86,7 +98,9 @@ def parse_star_file(file):
                         float(get_star_column_info(line, ang_res_col)),
                         float(get_star_column_info(line, fsc_corr_col)),
                         float(get_star_column_info(line, fsc_unmask_col)),
-                        float(get_star_column_info(line, fsc_mask_col))
+                        float(get_star_column_info(line, fsc_mask_col)),
+                        float(get_star_column_info(line, fsc_random_col)),
+                        float(phase_randomize_from)
                     )
                 )
 
@@ -141,10 +155,11 @@ def plot_data(data):
     fsc_corr_y_values = []
     fsc_unmask_y_values = []
     fsc_mask_y_values = []
+    fsc_random_y_values = []
     EST_RES = False
     for i in range(len(data)):
         ## expect data in form: tuple(inverse_resolution,  Ang_resolution,  FSC_corr,  FSC_unmasked,  FSC_masked)
-        inverse_resolution,  Ang_resolution,  FSC_corr,  FSC_unmasked,  FSC_masked = data[i]
+        inverse_resolution,  Ang_resolution,  FSC_corr,  FSC_unmasked,  FSC_masked, FSC_randomized, phase_radomize_from = data[i]
         x_values.append(inverse_resolution)
         fsc_corr_y_values.append(FSC_corr)
         if FSC_corr < 0.143 and EST_RES == False:
@@ -153,26 +168,37 @@ def plot_data(data):
             EST_RES_coord = (data[i-1][0], data[i-1][2])
         fsc_unmask_y_values.append(FSC_unmasked)
         fsc_mask_y_values.append(FSC_masked)
+        fsc_random_y_values.append(FSC_randomized)
 
+    plt.plot(x_values, fsc_random_y_values, label='Randomized', color = 'tab:purple', linewidth = 3)
     plt.plot(x_values, fsc_corr_y_values, label='Corrected', color = 'black', linewidth = 3)
     plt.plot(x_values, fsc_unmask_y_values, label='No Mask', color = 'tab:blue', linewidth = 3)
     plt.plot(x_values, fsc_mask_y_values, label='Masked', color = 'tab:red', linewidth = 3)
 
-
     # fig.canvas.draw()
     ax.set_xlim(xmin=0)
     locs,labels = plt.xticks()
-    ylocs,ylabels = plt.yticks()
-    ax.set_ylim(ymin=ylocs[0])
+    # ylocs,ylabels = plt.yticks()
+    # ax.set_ylim(ymin=ylocs[0])
+
+    ## draw a vertical line where phases were randomized from
+    if phase_radomize_from > 0:
+        plt.axvline(x = 1 / phase_radomize_from, color = 'tab:purple', label = None, alpha = 0.75, linewidth = 1.5, linestyle = '--')
+    ## on phase radomization from Steven Ludtke in google groups discussion:
+        # Step forward in time a bit, and you'll see that Richard Henderson published an additional paper suggesting a method for proving that your refinements really are independent, and adjusting your resolution (if it turns out that they were not). The idea is straightforward, take your raw data, do a normal (in EMAN2.1 this would be gold-standard) refinement, then phase-randomize the particle data beyond some target resolution. This resolution should be worse than the resolution your normal refinement claimed to have achieved. Since the particle data no longer has any actual data/signal past some cutoff resolution, if you re-refine it exactly the same way, you should find that the FSC falls rapidly at the exact resolution where you started randomizing the phases. If this does not happen, it implies that your refinement procedure has some hidden correlation between the even/odd maps, and your resolution was over-estimated.
+
+
+
     ## draw fsc 0.143 cutoff lines
-    OFFSET = 0.02 ## percent to offset the label from the point
-    point1 = [0, EST_RES_coord[1]]
-    point2 = [ EST_RES_coord[0], EST_RES_coord[1]]
-    point3 = [ EST_RES_coord[0], ylocs[0]]
-    plt.plot([point1[0], point2[0]], [point1[1], point2[1]], '-', color = 'black', alpha = 0.5)
-    plt.plot([point2[0], point3[0]], [point2[1], point3[1]], '-', color = 'black', alpha = 0.5)
-    plt.plot([point2[0]], [point2[1]], 'o', color = 'black', alpha = 0.5)
-    plt.annotate("%.2f Å" % EST_RES, (point2[0] + point2[0] * OFFSET, point2[1] + point2[1] * OFFSET))
+    plt.axhline(y = 0.143, color = 'black', label = None, alpha = 0.5, linewidth = 1.5, linestyle = '--')
+    # OFFSET = 0.02 ## percent to offset the label from the point
+    # point1 = [0, EST_RES_coord[1]]
+    # point2 = [ EST_RES_coord[0], EST_RES_coord[1]]
+    # point3 = [ EST_RES_coord[0], ylocs[0]]
+    # plt.plot([point1[0], point2[0]], [point1[1], point2[1]], '-', color = 'black', alpha = 0.5)
+    # plt.plot([point2[0], point3[0]], [point2[1], point3[1]], '-', color = 'black', alpha = 0.5)
+    # plt.plot([point2[0]], [point2[1]], 'o', color = 'black', alpha = 0.5)
+    # plt.annotate("%.2f Å" % EST_RES, (point2[0] + point2[0] * OFFSET, point2[1] + point2[1] * OFFSET))
 
 
     transformed_labels = []
@@ -186,7 +212,7 @@ def plot_data(data):
             transformed_labels.append("{:.2f}".format(res))
     ax.set_xticklabels(transformed_labels)
 
-    ax.legend(fontsize=12)
+    ax.legend(fontsize=10)
     plt.minorticks_on()
     plt.show()
 
