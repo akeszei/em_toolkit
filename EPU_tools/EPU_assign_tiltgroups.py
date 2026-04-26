d7a50c4c82f8baf1fe96ff37efbe9bff17cd78b7
Alexander Keszei <alex.keszei@gmail.com>
2026-04-26 16:38:23 -0400

    Update EPU_assign_tiltgroups.py

--- EPU_tools/EPU_assign_tiltgroups.py
+++ EPU_tools/EPU_assign_tiltgroups.py
@@ -56,6 +56,17 @@ def get_all_eer_files():
     print(" %s .eer files found in directory" % counter)
     return eerFiles
 
+def get_tiltgroup_from_fname(fname):
+    filename_to_list = fname.split("_") ## REF: https://forum.scilifelab.se/t/creating-optics-groups-from-epu-afis-data-and-more/122/7
+                                        ## FoilHole_<x>_Data_<y>_<tiltgroup>_<d>_<t>_EER.eer
+                                        ## Look for the second entry after 'Data'
+
+    idx_position_of_data = filename_to_list.index('Data')
+    relative_idx_position_from_data_to_tiltgrp = 2
+
+    tiltgroup_value = filename_to_list[idx_position_of_data + relative_idx_position_from_data_to_tiltgrp]
+
+    return tiltgroup_value
 
 def find_all_tiltgroups(fileList):
     """ Find all unique ID codes for each tilt group in the dataset and return them as a list for use:
@@ -63,12 +74,13 @@ def find_all_tiltgroups(fileList):
     """
     tilt_ids = []
     for file in fileList:
-        filename_to_list = file.split("_") ## REF: https://forum.scilifelab.se/t/creating-optics-groups-from-epu-afis-data-and-more/122/7
-                                           ## FoilHole_<x>_Data_<y>_<tiltgroup>_<d>_<t>_EER.eer
-                                           ## Look for the second entry after 'Data'
-        idx_position_of_data = filename_to_list.index('Data')
-        relative_idx_position_from_data_to_tiltgrp = 2
-        tilt_id = filename_to_list[idx_position_of_data + relative_idx_position_from_data_to_tiltgrp]
+        # filename_to_list = file.split("_") ## REF: https://forum.scilifelab.se/t/creating-optics-groups-from-epu-afis-data-and-more/122/7
+        #                                    ## FoilHole_<x>_Data_<y>_<tiltgroup>_<d>_<t>_EER.eer
+        #                                    ## Look for the second entry after 'Data'
+        # idx_position_of_data = filename_to_list.index('Data')
+        # relative_idx_position_from_data_to_tiltgrp = 2
+        # tilt_id = filename_to_list[idx_position_of_data + relative_idx_position_from_data_to_tiltgrp]
+        tilt_id = get_tiltgroup_from_fname(file)
         if not tilt_id in tilt_ids:
             tilt_ids.append(tilt_id)
     print(" %s tilt groups found in dataset" % len(tilt_ids))
@@ -90,9 +102,11 @@ def classify_images_into_tiltgroups(tiltgroupList, fileList):
         n += 1
         tilt_name = "tilt_%s" % n
         classified_dictionary[tilt_name] = []
+
     ## fill the dictionary with files based on matching tilt name
     for file in fileList:
-        file_tilt_id = file.split("_")[3]
+        # file_tilt_id = file.split("_")[3]
+        file_tilt_id = get_tiltgroup_from_fname(file)
         ## match the file to the index of the tilt group list
         matching_tilt_group = tiltgroupList.index(file_tilt_id) + 1
         matching_tilt_group_name = "tilt_%s" % matching_tilt_group
